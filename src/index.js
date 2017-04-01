5ae8665632b980544e2447a589ee3b4096850b35
PHANTOM <mohamad.d007@gmail.com>
2017-04-01 11:06:27 +0430

    first step configs

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import App from './components/App';
+
+const root = document.querySelector('#root');
 
 ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+  <App />, root
 );
